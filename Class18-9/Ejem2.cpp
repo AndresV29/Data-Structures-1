4013692b757d8fcb0e89ef2d4ddd620eed89721c
AndresV29 <andresvillalobos.carballo920@gmail.com>
2023-09-18 20:58:06 -0600

    example 3 if and switch

--- Class18-9/Ejem2.cpp
+++ Class18-9/Ejem2.cpp
@@ -2,12 +2,15 @@
 // Created by andre on 9/18/2023.
 //
 #include "iostream"
+#include <iomanip>
 #include "Ejem2.h"
 
-int main(){
+/*int main(){
     //Ejercicio 2 proyeccion de prestamos
     double amount, annInterest, payment;
     int years;
+    std::setw(2);
+    std::setprecision(5);
     std::cout<<"Ingrese monto total del prestamo: ";
     std::cin >> amount;
     std::cout<<"Ingrese interes anual: ";
@@ -17,4 +20,4 @@ int main(){
     payment = amount + (amount * (annInterest/100) * years);
     std::cout<<"El pago por mes es de:" << payment;
     return 0;
-}
+}*/
---
+++ Class18-9/Ejem3.h
@@ -0,0 +1,14 @@
+//
+// Created by andre on 9/18/2023.
+//
+
+#ifndef DATA_STRUCTURES_1_EJEM3_H
+#define DATA_STRUCTURES_1_EJEM3_H
+
+
+class Ejem3 {
+
+};
+
+
+#endif //DATA_STRUCTURES_1_EJEM3_H
