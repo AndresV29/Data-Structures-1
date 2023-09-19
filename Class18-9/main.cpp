ba94bdb050cfd83a3d1a03883f45722ca95db971
AndresV29 <andresvillalobos.carballo920@gmail.com>
2023-09-18 18:38:24 -0600

    clase 2 18-9

---
+++ Class18-9/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int edad;
+    //impresion en pantalla
+    std::cout << "Ingrese su edad:" << std::endl;
+    //input por consola
+    std::cin >> edad;
+    //concatenar string con valor de una variable
+    std::cout << "Su edad es: " << edad;
+    return edad;
+}
