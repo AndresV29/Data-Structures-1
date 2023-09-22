428ec883cb8ea792f9dc96cc0ec4a7f23dafbad3
AndresV29 <andresvillalobos.carballo920@gmail.com>
2023-09-22 15:35:17 -0600

    First excercise of practice 1 complete

---
+++ Practica1/Ejercicio1/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+ double calcularebaja(double salario, double impuesto){
+    double salarioRebajado, porcentaje;
+    //variable porcentaje se refiere al calculo del x porciento del salario ingresado
+    porcentaje = salario * impuesto;
+    salarioRebajado = salario - porcentaje;
+    cout << "Al salario: " <<salario<< "\n se le hizo una rebaja de:" << porcentaje << "\n Salario neto: " << salarioRebajado;
+     return salarioRebajado;
+}
+
+void salarioexhonerado(long salario){
+   cout << "Segun el salrio ingresado: "<< salario << ",este no debe pagar impuestos";
+}
+long impuesto(){
+    long salario;
+    cout << "Bienvenido al sistema de Impuestos \n Por favor ingrese el salario en Colones: ";
+    cin >> salario;
+    if (salario < 800000){
+        salarioexhonerado(salario);
+    }else if( salario > 800000 && salario < 1000000){
+        //Impuesto del 3.5%
+        calcularebaja(salario, 0.035);
+    }else if (salario >= 1000000 && salario < 1450000){
+        calcularebaja(salario, 0.085);
+    } else{
+        calcularebaja(salario, 0.1);
+    }
+
+    return salario;
+}
+int main() {
+    impuesto();
+    return 0;
+}
