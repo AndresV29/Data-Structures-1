bfa6835f95fa0ae2d8a353dd470658fe7bd032e2
AndresV29 <andresvillalobos.carballo920@gmail.com>
2023-10-16 19:12:34 -0600

    linked list simple

--- Class18-9/Class16-10/Listas.cpp
+++ Class18-9/Class16-10/Listas.cpp
@@ -3,3 +3,26 @@
 // clase 5 listas y pilas.
 
 #include "Listas.h"
+
+struct Nodo{
+    //dato que recibe la lista
+    int dato;
+    Nodo *sig;
+};
+
+void insertar(Nodo *head, int valor){
+    Nodo* nuevo = new Nodo;
+    nuevo->dato = valor;
+    nuevo->sig = head;
+    head = nuevo;
+}
+
+int main(){
+    Nodo* head = nullptr;
+
+    insertar(head, 10);
+    insertar(head, 20);
+    insertar(head, 30);
+    return 0;
+}
+
